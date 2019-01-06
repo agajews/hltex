4606983a2ad73ce4573d91a337f9d64a354d8af4
Alex Gajewski <agajews@gmail.com>
2019-01-06 08:56:41 -0800

    Fixed breaking change

--- hltex/translator.py
+++ hltex/translator.py
@@ -1,28 +1,57 @@
-class Command:
-    def __init__(self, name, translate_fn, nargs=0):
-        self.name = name
-        self.nargs = nargs
-        self.translate_fn = translate_fn
+class Arg:
+    def __init__(self, contents, optional=False):
+        self.contents = contents
+        self.optional = optional
 
-    def translate(self, args=None):
-        return self.translate_fn(args)
+
+def resolve_args(params, args):
+    all_args = []
+    arg_num = 0
+    for param in self.params:
+        if param == '!':
+            if arg_num < len(args) and not args[arg_num].optional:
+                all_args.append(args[arg_num])
+                arg_num += 1
+            elif arg_num >= len(args):
+                raise TranslationError('Missing required argument for command `{}`'.format(self.name))
+            else:
+                raise TranslationError('Superfluous optional argument provided to command `{}`'.format(self.name))
+        else:
+            if arg_num < len(args) and args[arg_num].optional:
+                all_args.append(args[arg_num])
+                arg_num += 1
+            else:
+                all_args.append(None)
+    return all_args
+
+class Command:
+    def __init__(self, name, translate_fn, params=''):
+        self.name = name
+        self.translate_fn = translate_fn
+        self.params = params
+        assert all([p in '!?' for p in params])
+
+    def translate(self, args):
+        return self.translate_fn(*resolve_args(self.params, args))
 
 
 class Environment:
-    def __init__(self, name, translate_fn):
+    def __init__(self, name, translate_fn, params=''):
         self.name = name
         self.translate_fn = translate_fn
+        self.params = params
+        assert all([p in '!?' for p in params])
 
-    def translate(self, body, opt=None):
-        return self.translate_fn(body, opt)
+    def translate(self, body, args):
+        return self.translate_fn(body, *resolve_args(self.params, args))
 
 
 def latex_cmd(name, args):
     return '\\%s%s' % (name, ''.join('{%s}' % arg for arg in args))
 
 
-def latex_env(name, before='', body='', after=''):
-    return '\\begin{%s}%s%s%s\\end{%s}' % (name, before, body, after, name)
+def latex_env(name, before='', body='', after='', args=''):
+    return '\\begin{%s}%s%s%s%s\\end{%s}' % (name, args, before, body, after, name)
 
 
 def translate_tbf(args):
@@ -30,7 +59,7 @@ def translate_tbf(args):
 
 
 commands = {
-    'tbf': Command('tbf', translate_tbf, nargs=1)
+    'tbf': Command('tbf', translate_tbf, params='!')
 }
 
 
@@ -42,7 +71,7 @@ def translate_eq(body, label):
 
 
 environments = {
-    'eq': Environment('eq', translate_eq)
+    'eq': Environment('eq', translate_eq, params='?')
 }
 
 
@@ -171,11 +200,13 @@ class Translator:
                 self.error('Too few arguments provided to command `{}`'.format(name))
         return command.translate(args)
 
-    def do_environment(self, name, for_document=False):
+    def do_environment(self, name, args, argstr, for_document=False):
         '''
         name: str representing the name of the current environment
+        args: a list of `Arg`s to pass to the environment if it's ours
+        argstr: a LaTeX string containing the arguments to pass if it's a LaTeX environment
         for_document: bool, True if parsing the `document` environment (for the first time), False otherwise
-        precondition: `self.pos` is at the first character after the name of the environment
+        precondition: `self.pos` is at the first character after the colon
             (e.g. a `:`, but also possible some whitespace or a non-alph character for one-liners)
         postcondition:
             for one-liner environments, `self.pos` is at the newline at the end of the line
@@ -184,7 +215,6 @@ class Translator:
                 on the (dedented) line following the block, or at `len(self.text)` if
                 the block is at the end of the file
         errors:
-            if the environment isn't followed by a colon
             if the line opening the environment is empty after the colon, but the following
                 line isn't indented and the environment isn't the first `document` environment
             if there is indentation not following the opening of an environment
@@ -192,30 +222,19 @@ class Translator:
                 not a whole repetition of `self.indent_str`)
         '''
         self.parse_while(iswhitespace)
-        opt = None
-        if self.text[self.pos] == '[':
-            self.pos += 1
-            body = self.extract_opt()
-            opt = body
-        self.parse_while(iswhitespace)
-        if self.text[self.pos] == ':':
-            self.pos += 1
-            self.parse_while(iswhitespace)
-            if self.text[self.pos] == '\n':
-                body = self.extract_block(for_environment=True, for_document=for_document)
-            else:
-                body_start = self.pos
-                body_end = self.text.find('\n', self.pos)
-                if body_end == -1:
-                    body_end = len(self.text)
-                self.pos = body_end
-                body = self.text[body_start:body_end]
-            if name in environments:
-                return environments[name].translate(body, opt)
-            else:
-                return latex_env(name, body=body)
+        if self.text[self.pos] == '\n':
+            body = self.extract_block(for_environment=True, for_document=for_document)
         else:
-            self.error('Environments must be followed by colons')
+            body_start = self.pos
+            body_end = self.text.find('\n', self.pos)
+            if body_end == -1:
+                body_end = len(self.text)
+            self.pos = body_end
+            body = self.text[body_start:body_end]
+        if name in environments:
+            return environments[name].translate(body, args)
+        else:
+            return latex_env(name, body=body, args=argstr)
 
     def get_control_seq(self):
         '''
@@ -232,74 +251,121 @@ class Translator:
         control_seq = self.text[control_start:self.pos]
         return control_seq
 
-    def extract_opt(self):
+    def extract_args(self, min_args = None, max_args=None):
         '''
-        precondition: `self.pos` is at the first character following the opening '['
-        postcondition: `self.pos` is at the first character following the closing `]`
-        errors: if there is an invalid character before the closing `]`
-        returns: the substring strictly between the opening and closing square brackets
+        min_args: an int representing the minumum number of arguments to parse; None for no minimum
+        max_args: an int representing the maximum number of arguments to parse; None for unlimited
+        precondition: `self.pos` is at the first character after the name of the command
+        postcondition: `self.pos` is at the first character after the last argument's closing brace or bracket
+        errors: if there are fewer than `min_args` arguments following the command
+        returns: a list of `Arg`s containing the parsed arguments, and a LaTeX string containing
+            the arguments in case they are for a LaTeX command
         '''
-        body_start = self.pos
-        self.parse_until(is_opt_end)
-        if self.text[self.pos] != ']':
-            self.error('Invalid character `{}` in optional parameter'.format(self.text[self.pos]))
-        body = self.text[body_start:self.pos]
-        self.pos += 1
-        return body
+        if max_args == 0:
+            return command.translate([])
+        args = []
+        argstr = ''
+        token_start = self.pos
+        nargs = 0
+        while True:
+            self.parse_while(iswhitespace)
+            if self.text[self.pos] == '{':  # TODO: make this less repetitive?
+                self.pos += 1
+                argstr += self.text[token_start:self.pos]
+                token_start = self.pos
+                arg = self.extract_arg(close='}')
+                argstr += arg + '}'
+                args.append(Arg(arg, optional=False))
+                nargs += 1
+            elif self.text[self.pos] == '[':
+                self.pos += 1
+                argstr += self.text[token_start:self.pos]
+                token_start = self.pos
+                arg = self.extract_arg(close=']')
+                argstr += arg + ']'
+                args.append(Arg(arg, optional=True))
+                nargs += 1
+            else:
+                if min_args is not None and nargs < min_args:
+                    self.error('Too few arguments provided')
+                else:
+                    break
+            if max_args is not None and nargs >= max_args:
+                break
+        return args, argstr
 
-    def extract_arg(self):
+    # def extract_opt(self):
+    #     '''
+    #     precondition: `self.pos` is at the first character following the opening '['
+    #     postcondition: `self.pos` is at the first character following the closing `]`
+    #     errors: if there is an invalid character before the closing `]`
+    #     returns: the substring strictly between the opening and closing square brackets
+    #     '''
+    #     body_start = self.pos
+    #     self.parse_until(is_opt_end)
+    #     if self.text[self.pos] != ']':
+    #         self.error('Invalid character `{}` in optional parameter'.format(self.text[self.pos]))
+    #     body = self.text[body_start:self.pos]
+    #     self.pos += 1
+    #     return body
+
+    def extract_arg(self, close='}'):
         '''
-        precondition: `self.pos` is at the first character following the opening '{'
-        postcondition: `self.pos` is at the first character following the closing '}';
+        close: the closing brace or bracket to the argument
+        precondition: `self.pos` is at the first character following the opening '{' or '['
+        postcondition: `self.pos` is at the first character following the closing '}' or ']';
             `extract_arg` ignores everything except for other commands inside the braces
-        errors: if there is a control sequence followed by a colon (i.e. an environment)
-            inside the braces
         returns: the substring strictly between the opening and closing curly braces
         '''
         token_start = self.pos
         body = ''
-        while self.pos < len(text):
-            self.parse_until(lambda c: c == '\\' or c == '}')
+        while self.pos < len(self.text):
+            self.parse_until(lambda c: c == '\\' or c == close)
             if self.text[self.pos] == '\\':
                 escape_start = self.pos
                 self.pos += 1
                 control_seq = self.get_control_seq()
-                if self.at_environment():
-                    self.error('You can\'t start an environment from a command body')
-                elif control_seq in commands:
-                    body += self.text[token_start:escape_start]
+                # if self.at_environment():
+                #     self.error('You can\'t start an environment from a command body')
+                body += self.text[token_start:escape_start]
+                if control_seq in commands:
                     body += self.do_command(control_seq)
-                    token_start = self.pos
+                else:
+                    _, argstr = self.extract_args()
+                    body += '\\' + control_seq + argstr
+                    # TODO: issue a warning if the current character is a colon
+                token_start = self.pos
             else:
                 body += self.text[token_start:self.pos]
                 self.pos += 1
             return body
 
-    def at_environment(self):
-        # TODO: expand this to allow for other kinds of arguments
-        # TODO: add support for '\\:' for literal colons
-        '''
-        precondition: `self.pos` is at the first character following the name of a control sequence
-        postcondition: `self.pos` is in the same place where it started
-        returns: True if the control sequence is followed by a colon (after optionally some arguments),
-            indicating it is an environment, False otherwise
-        '''
-        old_pos = self.pos
-        self.parse_while(iswhitespace)
-        # print('checking at environment')
-        # print(self.text[self.pos:self.pos+10])
-        if self.text[self.pos] == '[':
-            self.parse_until(is_opt_end)
-            if self.text[self.pos] != ']':
-                self.pos = old_pos
-                return False
-            self.pos += 1
-        self.parse_while(iswhitespace)
-        if self.text[self.pos] == ':':
-            self.pos = old_pos
-            return True
-        self.pos = old_pos
-        return False
+    # def at_environment(self):
+    #     # TODO: expand this to allow for other kinds of arguments
+    #     # TODO: add support for '\\:' for literal colons
+    #     '''
+    #     precondition: `self.pos` is at the first character following the name of a control sequence
+    #     postcondition: `self.pos` is in the same place where it started
+    #     returns: True if the control sequence is followed by a colon (after optionally some arguments),
+    #         indicating it is an environment, False otherwise
+    #     '''
+    #     old_pos = self.pos
+    #     self.parse_while(iswhitespace)
+    #     # print('checking at environment')
+    #     # print(self.text[self.pos:self.pos+10])
+    #     while self.pos < len(self.text):
+    #     if self.text[self.pos] == '[':
+    #         self.parse_until(is_opt_end)
+    #         if self.text[self.pos] != ']':
+    #             self.pos = old_pos
+    #             return False
+    #         self.pos += 1
+    #     self.parse_while(iswhitespace)
+    #     if self.text[self.pos] == ':':
+    #         self.pos = old_pos
+    #         return True
+    #     self.pos = old_pos
+    #     return False
 
     def extract_block(self, for_environment=False, for_document=False):
         # TODO: can this be broken up into smaller methods?
@@ -309,6 +375,7 @@ class Translator:
         postcondition: `self.pos` is at the first non-whitespace character of the line
             following the block, or at `len(self.text)` if the block is at the end of the file
         errors:
+            if there is an ill-formed environment (e.g. not indended after and not a one-liner)
             if there is indentation not following the opening of an environment
             if any indentation is ill-formed (e.g. not all tabs or spaces, or
                 not a whole repetition of `self.indent_str`)
@@ -356,25 +423,32 @@ class Translator:
                 self.pos += 1
                 control_seq = self.get_control_seq()
                 # print(control_seq)
-                if self.at_environment():  # TODO: make this more concise?
-                    # print('at environment')
-                    body += self.text[token_start:escape_start]
-                    for_document = False
-                    if not self.in_document and control_seq == 'document':
-                        self.in_document = True
-                        for_document = True
-                    # print('Doing environment `{}`'.format(control_seq))
-                    body += self.do_environment(control_seq, for_document=for_document)
-                    token_start = self.pos
-                elif control_seq in commands:
-                    body += self.text[token_start:escape_start]
+                body += self.text[token_start:escape_start]
+                if control_seq in commands:
                     body += self.do_command(control_seq)
-                    token_start = self.pos
+                else:
+                    args, argstr = self.extract_args()
+                    whitespace_start = self.pos
+                    self.parse_while(iswhitespace)
+                    if self.text[self.pos] == ':':
+                        self.pos += 1
+                        # print('at environment')
+                        for_document = False
+                        if not self.in_document and control_seq == 'document':
+                            self.in_document = True
+                            for_document = True
+                        # print('Doing environment `{}`'.format(control_seq))
+                        body += self.do_environment(control_seq, args, argstr, for_document=for_document)
+                        token_start = self.pos
+                    else:
+                        body += '\\' + control_seq + argstr + self.text[whitespace_start:self.pos]
+                token_start = self.pos
         body += self.text[token_start:]
         return body
 
 
     def translate(self):
+        # import pdb;pdb.set_trace()
         try:
             return self.extract_block()
         except TranslationError as e:
